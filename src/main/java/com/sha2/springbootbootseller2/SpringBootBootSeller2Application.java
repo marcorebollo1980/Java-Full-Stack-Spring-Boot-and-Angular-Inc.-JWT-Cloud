d1a88982e33eaa91172ca9aa2634ea362aaa462b
marcorebollo1980 <marcorebollolopez@gmail.com>
2022-09-28 07:28:18 -0500

    commit 1
    commit1

---
+++ src/main/java/com/sha2/springbootbootseller2/SpringBootBootSeller2Application.java
@@ -0,0 +1,13 @@
+package com.sha2.springbootbootseller2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBootSeller2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootBootSeller2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/sha2/springbootbootseller2/SpringBootBootSeller2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sha2.springbootbootseller2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootBootSeller2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
